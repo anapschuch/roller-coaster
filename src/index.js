4538d32e26bf7b6effaf31b945fc4e81030073db
Ana Paula Schuch <aplschuch@gmail.com>
2021-11-13 01:44:56 -0300

    Adicionado percurso da montanha russa

---
+++ src/index.js
@@ -0,0 +1,117 @@
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import car from './car';
+
+const canvas = document.querySelector('canvas');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas: canvas,
+  alpha: true
+});
+
+function setView() {
+  // Definição do tamanho da vista
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+
+setView();
+window.addEventListener('resize', setView);
+
+var controls = new OrbitControls(camera, renderer.domElement);
+
+var plane = new THREE.GridHelper(60, 10);
+scene.add(plane);
+
+const loader = new OBJLoader();
+var curvePath = undefined;
+
+loader.load(
+  '/roller-coaster-curve.obj',
+  // called when resource is loaded
+  function (object) {
+    var curveGeometry = object.children[0].geometry;
+    console.log(curveGeometry);
+    const coordinates = curveGeometry.getAttribute('position');
+    console.log(coordinates);
+    var points = [];
+
+    for (var i = 0; i < 3 * coordinates.count; i += 3) {
+      points.push(
+        new THREE.Vector3(
+          coordinates.array[i],
+          coordinates.array[i + 1],
+          coordinates.array[i + 2]
+        )
+      );
+    }
+    curvePath = new THREE.CatmullRomCurve3(points);
+  }
+);
+
+loader.load(
+  '/roller-coaster.obj',
+  // called when resource is loaded
+  function (object) {
+    var curveGeometry = object.children[0].geometry;
+    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    const line = new THREE.Line(curveGeometry, material);
+    scene.add(line);
+  }
+);
+
+var counter = 0;
+scene.add(car);
+var mustRotate = false;
+var rotateTwice = false;
+
+function animateCar() {
+  if (curvePath) {
+    counter += 0.001;
+
+    counter %= 1;
+    var p1 = curvePath.getPointAt(counter);
+    var p2 = curvePath.getPointAt((counter + 0.01) % 1);
+
+    car.position.set(p1.x, p1.y, p1.z);
+    car.lookAt(p2.x, p2.y, p2.z);
+
+    var p3 = curvePath.getPointAt((counter - 0.01 + 1) % 1);
+    if (
+      ((p3.x > p1.x && p2.x > p1.x) || (p3.x < p1.x && p2.x < p1.x)) &&
+      Math.abs(p2.y - p3.y) > 2
+    ) {
+      if (!rotateTwice) {
+        console.log('a');
+        mustRotate = !mustRotate;
+        rotateTwice = true;
+      }
+    } else rotateTwice = false;
+
+    if (mustRotate) {
+      car.rotateX(-Math.PI);
+      car.rotateY(Math.PI);
+    }
+  }
+}
+
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  controls.update();
+  animateCar();
+}
+animate();
+
+camera.position.x = 0;
+camera.position.y = 20;
+camera.position.z = -25;
