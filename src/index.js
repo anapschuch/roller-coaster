277e63cf547080e68f5577256d97e8fe8443c337
Diego Fidalgo <diego.fidalgo2@gmail.com>
2021-11-13 13:43:37 -0300

    Adicionada tela de carregamento

--- src/index.js
+++ src/index.js
@@ -3,89 +3,92 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import car from './car';
 
-const canvas = document.querySelector('canvas');
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight
-);
-
-const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  canvas: canvas,
-  alpha: true
-});
-
-function setView() {
-  // Definição do tamanho da vista
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+if (import.meta.env.PROD) {
+  // Ignorar todos os console.log no build final
+  console.log = () => { /* ignore */ };
 }
 
-setView();
-window.addEventListener('resize', setView);
-
-let controls = new OrbitControls(camera, renderer.domElement);
-
-let plane = new THREE.GridHelper(60, 10);
-scene.add(plane);
+const scene = new THREE.Scene();
 
 const loader = new OBJLoader();
-let curvePath = undefined;
 
-loader.load(
-  '/roller-coaster-curve.obj',
-  // called when resource is loaded
-  function (object) {
-    let curveGeometry = object.children[0].geometry;
-    console.log(curveGeometry);
-    const coordinates = curveGeometry.getAttribute('position');
-    console.log(coordinates);
-    let points = [];
+async function main() {
+  // Primeiro, esperamos que todos os objetos sejam carregados
+  const [curvePath, curveLine] = await Promise.all([
+    loader.loadAsync('/roller-coaster-curve.obj').then(object => {
+      const curveGeometry = object.children[0].geometry;
+      console.log(curveGeometry);
+      const coordinates = curveGeometry.getAttribute('position');
+      console.log(coordinates);
+      const points = [];
 
-    for (let i = 0; i < 3 * coordinates.count; i += 3) {
-      points.push(
-        new THREE.Vector3(
-          coordinates.array[i],
-          coordinates.array[i + 1],
-          coordinates.array[i + 2]
-        )
-      );
-    }
-    curvePath = new THREE.CatmullRomCurve3(points);
+      for (let i = 0; i < 3 * coordinates.count; i += 3) {
+        points.push(
+          new THREE.Vector3(
+            coordinates.array[i],
+            coordinates.array[i + 1],
+            coordinates.array[i + 2]
+          )
+        );
+      }
+      return new THREE.CatmullRomCurve3(points);
+    }),
+    loader.loadAsync('/roller-coaster.obj').then(object => {
+      const curveGeometry = object.children[0].geometry;
+      const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+
+      return new THREE.Line(curveGeometry, material);
+    })
+  ]);
+
+  document.getElementById('loading').remove();
+
+  const canvas = document.querySelector('canvas');
+
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight
+  );
+
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    canvas: canvas,
+    alpha: true
+  });
+
+  function setView() {
+    // Definição do tamanho da vista
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
   }
-);
 
-loader.load(
-  '/roller-coaster.obj',
-  // called when resource is loaded
-  function (object) {
-    let curveGeometry = object.children[0].geometry;
-    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
-    const line = new THREE.Line(curveGeometry, material);
-    scene.add(line);
-  }
-);
+  setView();
+  window.addEventListener('resize', setView);
 
-let counter = 0;
-scene.add(car);
-let mustRotate = false;
-let rotateTwice = false;
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-function animateCar() {
-  if (curvePath) {
+  const plane = new THREE.GridHelper(60, 10);
+
+  scene.add(plane);
+  scene.add(car);
+  scene.add(curveLine);
+
+  let counter = 0;
+  let mustRotate = false;
+  let rotateTwice = false;
+
+  function animateCar() {
     counter += 0.001;
 
     counter %= 1;
-    let p1 = curvePath.getPointAt(counter);
-    let p2 = curvePath.getPointAt((counter + 0.01) % 1);
+    const p1 = curvePath.getPointAt(counter);
+    const p2 = curvePath.getPointAt((counter + 0.01) % 1);
 
     car.position.set(p1.x, p1.y, p1.z);
     car.lookAt(p2.x, p2.y, p2.z);
 
-    let p3 = curvePath.getPointAt((counter - 0.01 + 1) % 1);
+    const p3 = curvePath.getPointAt((counter - 0.01 + 1) % 1);
     if (
       ((p3.x > p1.x && p2.x > p1.x) || (p3.x < p1.x && p2.x < p1.x)) &&
       Math.abs(p2.y - p3.y) > 2
@@ -102,16 +105,19 @@ function animateCar() {
       car.rotateY(Math.PI);
     }
   }
+
+  function animate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    controls.update();
+    animateCar();
+  }
+
+  animate();
+
+  camera.position.x = 0;
+  camera.position.y = 20;
+  camera.position.z = -25;
 }
 
-function animate() {
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-  controls.update();
-  animateCar();
-}
-animate();
-
-camera.position.x = 0;
-camera.position.y = 20;
-camera.position.z = -25;
+main();
